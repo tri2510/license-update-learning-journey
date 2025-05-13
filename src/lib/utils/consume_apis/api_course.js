c85a71e0136b1a9f022db1fdfc32943925481927
Nhan Luong <130432957+NhanLuongBGSV@users.noreply.github.com>
2025-05-13 20:32:11 +0700

    Update api_course.js
    Signed-off-by: Nhan Luong <130432957+NhanLuongBGSV@users.noreply.github.com>

--- src/lib/utils/consume_apis/api_course.js
+++ src/lib/utils/consume_apis/api_course.js
@@ -1,7 +1,7 @@
 async function fetchCourseBySlug(slug) {
     if (!slug) throw ('Invalid post slug');
     try {
-        const response = (await fetch((process.env.HOST || 'http://localhost:3000') + "/api/courses/" + slug))
+        const response = (await fetch(process.env.HOST + "/api/courses/" + slug))
         const data = await response.json();
         if (data && data.success) {
             return data.data
