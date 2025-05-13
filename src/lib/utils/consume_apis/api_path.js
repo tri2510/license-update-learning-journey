7402a02dd6c4dc9bd10615c76fbb05fc4a066efd
Nhan Luong <130432957+NhanLuongBGSV@users.noreply.github.com>
2025-05-13 20:32:33 +0700

    Update api_path.js
    Signed-off-by: Nhan Luong <130432957+NhanLuongBGSV@users.noreply.github.com>

--- src/lib/utils/consume_apis/api_path.js
+++ src/lib/utils/consume_apis/api_path.js
@@ -1,6 +1,6 @@
 async function fetchPaths() {
     try {
-        const response = (await fetch((process.env.HOST || 'http://localhost:3000') + "/api/paths"))
+        const response = (await fetch(process.env.HOST + "/api/paths"))
         const data = await response.json();
         if (data && data.success) {
             return data.data
@@ -16,7 +16,7 @@ async function fetchPaths() {
 async function fetchPathBySlug(slug) {
     if (!slug) throw ('Invalid post slug');
     try {
-        const response = (await fetch((process.env.HOST || 'http://localhost:3000') + "/api/paths/" + slug))
+        const response = (await fetch(process.env.HOST + "/api/paths/" + slug))
         const data = await response.json();
         if (data && data.success) {
             return data.data
